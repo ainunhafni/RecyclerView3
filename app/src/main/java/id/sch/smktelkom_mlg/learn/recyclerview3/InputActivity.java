5dcd59597ee7e9407c8853cce43eae142708057a
Ainun Hafni <ainunlia64@gmail.com>
2016-12-03 17:04:26 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
@@ -30,6 +30,17 @@ public class InputActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
+        hotel = (Hotel) getIntent().getSerializableExtra(MainActivity.HOTEL);
+        if (hotel!=null)
+        {
+            setTitle("Edit "+hotel.judul);
+            fillData();
+        }
+        else
+        {
+            setTitle("New Hotel");
+        }
+
         etJudul = (EditText) findViewById(R.id.editTextNama);
         etDeskripsi = (EditText) findViewById(R.id.editTextDeskripsi);
         etDetail = (EditText) findViewById(R.id.editTextDetail);
@@ -55,6 +66,16 @@ public class InputActivity extends AppCompatActivity
                     }
                 }
         );
+    }
+
+    private void fillData()
+    {
+        etJudul.setText(hotel.judul);
+        etDeskripsi.setText(hotel.deskripsi);
+        etDetail.setText(hotel.detail);
+        etLokasi.setText(hotel.lokasi);
+        uriFoto = Uri.parse(hotel.foto);
+        ivFoto.setImageURI(uriFoto);
     }
 
     private void doSave()
